9db7afd605a5e9cd3e51f0c0313de05ea9403da1
im-Debabrata <96492019+ImDebabrata@users.noreply.github.com>
2024-12-29 15:20:03 +0530

    initial auth pages

---
+++ src/constrants/ImageConfig.tsx
@@ -0,0 +1,10 @@
+import signinImage from "@/assets/login-image.jpg";
+import signupImage from "@/assets/signup-image.jpg";
+import avatarPlaceholder from "@/assets/avatar-placeholder.png";
+const ImageConfig = {
+  signinImage,
+  signupImage,
+  avatarPlaceholder,
+};
+
+export default ImageConfig;
