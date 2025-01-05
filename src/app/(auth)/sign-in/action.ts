2ef8278790b3278f2e6707348696214a49a4387a
im-Debabrata <96492019+ImDebabrata@users.noreply.github.com>
2025-01-05 20:15:15 +0530

    refactor: added try catch in login/signup actions

--- src/app/(auth)/sign-in/action.ts
+++ src/app/(auth)/sign-in/action.ts
@@ -4,38 +4,47 @@ import prisma from "@/lib/prisma";
 import { createSession } from "@/lib/stateless-session";
 import { loginSchema, LoginValues } from "@/lib/validation";
 import { compare } from "bcryptjs";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 export async function signin(
   credentials: LoginValues
 ): Promise<{ error?: string; success?: string }> {
-  // 1. Validate fields
-  const { username, password } = loginSchema.parse(credentials);
+  try {
+    // 1. Validate fields
+    const { username, password } = loginSchema.parse(credentials);
 
-  // 2. Check is user available
-  const existingUser = await prisma.user.findFirst({
-    where: {
-      username: {
-        equals: username,
-        mode: "insensitive",
+    // 2. Check is user available
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
       },
-    },
-  });
+    });
 
-  if (!existingUser || !existingUser.passwordHash) {
+    if (!existingUser || !existingUser.passwordHash) {
+      return {
+        error: "Incorrect username or password!",
+      };
+    }
+
+    const isValidPassword = await compare(password, existingUser.passwordHash);
+    if (!isValidPassword) {
+      return {
+        error: "Incorrect username or password",
+      };
+    }
+    await createSession(existingUser.id);
+
     return {
-      error: "Incorrect username or password!",
+      success: "Login Success",
+    };
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.log(error);
+    return {
+      error: "Something went wrong!",
     };
   }
-
-  const isValidPassword = await compare(password, existingUser.passwordHash);
-  if (!isValidPassword) {
-    return {
-      error: "Incorrect username or password",
-    };
-  }
-  await createSession(existingUser.id);
-
-  return {
-    success: "Login Success",
-  };
 }
