c5cbae45ff6d18f674c07fdd87d9ee2da2ca43ea
im-Debabrata <96492019+ImDebabrata@users.noreply.github.com>
2025-01-07 00:46:13 +0530

    chore: allowing render image from any host

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+/*
+https://nextjs.org/docs/messages/next-image-unconfigured-host
+images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.example.com',
+        port: '',
+        pathname: '/account123/**',
+        search: '',
+      },
+    ],
+  },
+*/
