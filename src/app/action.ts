9ff244965e9d919d16266f40d2ae863d7894019e
im-Debabrata <96492019+ImDebabrata@users.noreply.github.com>
2025-01-07 00:46:52 +0530

    feat: get current user details from cookies

--- src/app/action.ts
+++ src/app/action.ts
@@ -2,8 +2,17 @@
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import RouteConfig from "@/constrants/RouteConfig";
+import { decrypt } from "@/lib/stateless-session";
+import { SessionPayload } from "@/lib/validation";
 
 export async function handleLogOut() {
   (await cookies()).delete("session");
   redirect(RouteConfig.authScreens.SIGN_IN);
+}
+
+export async function getCurrentUser(): Promise<SessionPayload | null> {
+  const cookie = (await cookies()).get("session")?.value;
+  const user = (await decrypt(cookie)) as SessionPayload | null;
+  console.log("the file is running",new Date());
+  return { ...user, avatarUrl: user?.avatarUrl || "" } as SessionPayload | null;
 }
--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -10,7 +10,8 @@ import {
 } from "./ui/dropdown-menu";
 import UserAvatar from "./UserAvatar";
 import { LogOutIcon, UserIcon } from "lucide-react";
-import { handleLogOut } from "@/app/action";
+import { getCurrentUser, handleLogOut } from "@/app/action";
+import { useQuery } from "@tanstack/react-query";
 
 interface UserButtonProps {
   className?: string;
@@ -18,11 +19,17 @@ interface UserButtonProps {
 
 function UserButton(props: UserButtonProps) {
   const { className } = props;
-  //   console.log(document.cookie,'the cookie')
+
+  const { data } = useQuery({
+    queryKey: ["current user info"],
+    staleTime: Infinity,
+    queryFn: getCurrentUser,
+  });
+  const { avatarUrl = "" } = data || {};
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <UserAvatar size={40} avatarUrl={""} className={className} />
+        <UserAvatar size={40} avatarUrl={avatarUrl} className={className} />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
@@ -31,7 +38,7 @@ function UserButton(props: UserButtonProps) {
           <UserIcon className="mr-2 size-4" />
           Profile
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={()=>handleLogOut()}>
+        <DropdownMenuItem onClick={() => handleLogOut()}>
           <LogOutIcon className="mr-2 size-4" />
           Logout
         </DropdownMenuItem>
