78704b312233eca086cb2fc63b511e652f08878b
im-Debabrata <96492019+ImDebabrata@users.noreply.github.com>
2025-01-07 18:23:46 +0530

    feat: responsive menubar

---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import RouteConfig from "@/constrants/RouteConfig";
+import { Bell, Bookmark, Home, Mail } from "lucide-react";
+
+interface MenuBarProps {
+  className?: string;
+}
+
+const buttonItems = [
+  {
+    title: "Home",
+    icon: <Home />,
+    route: RouteConfig.protectedRoute.MAIN_SCREEN,
+  },
+  {
+    title: "Notificatios",
+    icon: <Bell />,
+    route: "",
+  },
+  {
+    title: "Messages",
+    icon: <Mail />,
+    route: "",
+  },
+  {
+    title: "Bookmarks",
+    icon: <Bookmark />,
+    route: "",
+  },
+];
+
+function MenuBar(props: MenuBarProps) {
+  const { className } = props;
+  return (
+    <div className={className}>
+      {buttonItems.map((buttonConfig, index) => {
+        return (
+          <Button
+            key={index}
+            variant={"ghost"}
+            className="flex items-center justify-start gap-3"
+            title={buttonConfig.title}
+            asChild
+          >
+            <Link href={buttonConfig.route}>
+              {buttonConfig.icon}
+              <span className="hidden lg:inline">{buttonConfig.title}</span>
+            </Link>
+          </Button>
+        );
+      })}
+    </div>
+  );
+}
+
+export default MenuBar;
