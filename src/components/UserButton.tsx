42d3529ba623a90d3804ae5debc467530edf3852
im-Debabrata <96492019+ImDebabrata@users.noreply.github.com>
2025-01-07 17:28:42 +0530

    feat: theme toggle in user menu

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -5,19 +5,26 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import UserAvatar from "./UserAvatar";
-import { LogOutIcon, UserIcon } from "lucide-react";
+import { Check, LogOutIcon, Monitor, Moon, Sun, UserIcon } from "lucide-react";
 import { getCurrentUser, handleLogOut } from "@/app/action";
 import { useQuery } from "@tanstack/react-query";
+import { useTheme } from "next-themes";
 
 interface UserButtonProps {
   className?: string;
 }
 
 function UserButton(props: UserButtonProps) {
+  const { theme, setTheme } = useTheme();
+
   const { className } = props;
 
   const { data } = useQuery({
@@ -38,6 +45,31 @@ function UserButton(props: UserButtonProps) {
           <UserIcon className="mr-2 size-4" />
           Profile
         </DropdownMenuItem>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <Monitor className="mr-2 size-4" />
+            Theme
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                <Sun className="mr-2 size-4" />
+                Light
+                {theme === "light" && <Check className="mr-2 size-4" />}
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                <Moon className="mr-2 size-4" />
+                Dark
+                {theme === "dark" && <Check className="mr-2 size-4" />}
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                <Monitor className="mr-2 size-4" />
+                System
+                {theme === "system" && <Check className="mr-2 size-4" />}
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
         <DropdownMenuItem onClick={() => handleLogOut()}>
           <LogOutIcon className="mr-2 size-4" />
           Logout
