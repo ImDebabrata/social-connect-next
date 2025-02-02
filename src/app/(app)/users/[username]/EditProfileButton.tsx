f0e73b7178245fb4c62837173651fd982f79fc2e
im-Debabrata <96492019+ImDebabrata@users.noreply.github.com>
2025-02-02 21:05:55 +0530

    feat: update user profile

---
+++ src/app/(app)/users/[username]/EditProfileButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { UserData } from "@/lib/types";
+import React, { useState } from "react";
+import EditProfileDialog from "./EditProfileDialog";
+
+interface EditProfileButtonProps {
+  user: UserData;
+}
+
+function EditProfileButton(props: EditProfileButtonProps) {
+  const { user } = props;
+  const [showDialog, setShowDialog] = useState(false);
+  return (
+    <>
+      <Button variant={"outline"} onClick={() => setShowDialog(true)}>
+        Edit Profile
+      </Button>
+      <EditProfileDialog
+        user={user}
+        open={showDialog}
+        onOpenChange={setShowDialog}
+      />
+    </>
+  );
+}
+
+export default EditProfileButton;
