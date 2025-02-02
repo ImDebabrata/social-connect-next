37bff2de183b86eddfcef8f7505e607781b2ed3a
im-Debabrata <96492019+ImDebabrata@users.noreply.github.com>
2025-02-02 11:05:19 +0530

    feat: user tooltip hover

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -8,6 +8,7 @@ import { unstable_cache } from "next/cache";
 import { formatNumber } from "@/lib/utils";
 import FollowButton from "./FollowButton";
 import { getUserDataSelect } from "@/lib/types";
+import UserTooltip from "./UserTooltip";
 
 function TrendsSidebar() {
   return (
@@ -24,18 +25,18 @@ export default TrendsSidebar;
 
 async function WhoToFollow() {
   const user = await getCurrentUser();
-//   await new Promise((r) => setTimeout(r, 10000));
+  //   await new Promise((r) => setTimeout(r, 10000));
   if (!user) return null;
   const usersToFollow = await prisma.user.findMany({
     where: {
       NOT: {
         id: user.userId,
       },
-      followers:{
-        none:{
-          followerId:user.userId
-        }
-      }
+      followers: {
+        none: {
+          followerId: user.userId,
+        },
+      },
     },
     select: getUserDataSelect(user.userId),
     take: 5,
@@ -46,25 +47,32 @@ async function WhoToFollow() {
       <div className="text-xl font-bold">Who to follow</div>
       {usersToFollow.map((user) => (
         <div key={user.id} className="flex items-center justify-between gap-3">
-          {/* Todo: redirect ro actual user page */}
-          <Link href={""} className="flex items-center gap-3">
-            <UserAvatar
-              avatarUrl={user.avatarUrl}
-              className="flex-none"
-            ></UserAvatar>
-            <div>
-              <p className="line-clamp-1 break-all font-semibold hover:underline">
-                {user.displayName}
-              </p>
-              <p className="line-clamp-1 break-all text-muted-foreground">
-                @{user.username}
-              </p>
-            </div>
-          </Link>
-          <FollowButton userId={user.id} initialState={{
-            followers:user._count.followers,
-            isFollowedByUser:!!user.followers.some(({followerId})=>followerId===user.id)
-          }}/>
+          <UserTooltip user={user}>
+            {/* Todo: redirect ro actual user page */}
+            <Link href={""} className="flex items-center gap-3">
+              <UserAvatar
+                avatarUrl={user.avatarUrl}
+                className="flex-none"
+              ></UserAvatar>
+              <div>
+                <p className="line-clamp-1 break-all font-semibold hover:underline">
+                  {user.displayName}
+                </p>
+                <p className="line-clamp-1 break-all text-muted-foreground">
+                  @{user.username}
+                </p>
+              </div>
+            </Link>
+          </UserTooltip>
+          <FollowButton
+            userId={user.id}
+            initialState={{
+              followers: user._count.followers,
+              isFollowedByUser: !!user.followers.some(
+                ({ followerId }) => followerId === user.id
+              ),
+            }}
+          />
         </div>
       ))}
     </div>
