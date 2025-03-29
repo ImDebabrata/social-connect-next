d222b029544747bfe7879f4fe737a69b54ac7f95
im-Debabrata <96492019+ImDebabrata@users.noreply.github.com>
2025-03-29 20:33:49 +0530

    feat: delete comment feature

--- src/components/comments/mutation.ts
+++ src/components/comments/mutation.ts
@@ -5,7 +5,7 @@ import {
   useMutation,
   useQueryClient,
 } from "@tanstack/react-query";
-import { submitComment } from "./action";
+import { deleteComment, submitComment } from "./action";
 import { CommentsPage } from "@/lib/types";
 
 export function useSubmitCommentMutation(postId: string) {
@@ -59,6 +59,49 @@ export function useSubmitCommentMutation(postId: string) {
       toast({
         variant: "destructive",
         description: "Failed to add comment. Please try again.",
+      });
+    },
+  });
+
+  return mutation;
+}
+
+export function useDeleteCommentMutation() {
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: deleteComment,
+    onSuccess: async (deletedComment) => {
+      const queryKey: QueryKey = ["comments", deletedComment.postId];
+
+      await queryClient.cancelQueries({ queryKey });
+
+      queryClient.setQueryData<InfiniteData<CommentsPage, string | null>>(
+        queryKey,
+        (oldData) => {
+          if (!oldData) return undefined;
+          return {
+            pageParams: oldData.pageParams,
+            pages: oldData.pages.map((page) => ({
+              previousCursor: page.previousCursor,
+              comments: page.comments.filter(
+                (comment) => comment.id !== deletedComment.id
+              ),
+            })),
+          };
+        }
+      );
+
+      toast({
+        description: "Comment deleted successfully",
+      });
+    },
+    onError: (error) => {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Failed to delete comment. Please try again.",
       });
     },
   });
