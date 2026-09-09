8c4bcab195c4cac5cfb2c9fd25a459f563f0fc83
Debabrata Datta <debabratakgt@hotmail.com>
2026-09-09 21:58:37 +0530

    chore: add eslint-config-prettier to prevent ESLint/Prettier conflicts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -17,6 +18,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "warn",
     },
   },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
